571854af09e2948b9078a177e2b867bc3440eff5
Logan Henson <logan@loganhenson.com>
2021-06-23 09:23:53 -0500

    Add nprogress.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+import Nprogress from "nprogress";
+
+let loadingTimeout = null;
+Nprogress.configure({ showSpinner: false, minimum: 0.4 });
+window.addEventListener("elm-loading", function({ detail: loading }) {
+    clearTimeout(loadingTimeout);
+
+    if (loading) {
+        loadingTimeout = setTimeout(Nprogress.start, 180);
+    } else {
+        Nprogress.done();
+    }
+});
